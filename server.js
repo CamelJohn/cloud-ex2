b77b65ebd46cfeb1de1aef4eda846cd595499deb
EC2 Default User <ec2-user@ip-172-31-32-69.ec2.internal>
2020-07-28 14:11:34 +0000

    ex2

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output" : "Hello World!"
+    "Output" : "Hello World!",
+    "descriptor" : "get route"
   });
 });
 
 app.post('/', function(req, res) {
+  console.log(curlData);
   res.send({
-    "Output" : "Hello World!"
+    "Output" : "Hello World!",
+    "descriptor" : "post route",
+    "curlData": curlData
   });
 });
 
