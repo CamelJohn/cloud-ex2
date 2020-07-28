a1bbf553c9d8b1cea92115f4ba632570f06d215f
EC2 Default User <ec2-user@ip-172-31-32-69.ec2.internal>
2020-07-28 15:16:53 +0000

    done ex3 with the same instance and fixed passing tests

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var port = process.env.PORT || 8080;
 var app = express();
+var cors = require('cors');
+
+app.use(cors());
+
+var obj = [
+    { "id": 1, "title": "hello", "body" : "world"},
+    { "id": 2, "title": "hello", "body" : "world"},
+    { "id": 3, "title": "hello", "body" : "world"},
+    { "id": 4, "title": "hello", "body" : "world"},
+  ];
 
 app.get('/', function(req, res) {
   res.send({
@@ -9,12 +19,14 @@ app.get('/', function(req, res) {
   });
 });
 
+app.get('/generics', function(req, res) {
+  res.send(JSON.stringify(obj))
+})
+
 app.post('/', function(req, res) {
-  console.log(curlData);
   res.send({
     "Output" : "Hello World!",
     "descriptor" : "post route",
-    "curlData": curlData
   });
 });
 
